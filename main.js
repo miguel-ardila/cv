d3d84a51bd1d72d83a4dd43caef77e7c2d0010c3
Miguel Ardila <miguel_sxe@hotmail.com>
2023-07-28 20:29:47 -0500

    configuracion de ingles a espanol

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("Hi! I`m Miguel");
+function toggleLanguage() {
+    const body = document.body;
+    const currentLang = body.getAttribute("lang");
+    const newLang = currentLang === "es" ? "en" : "es";
+    body.setAttribute("lang", newLang);
+}
